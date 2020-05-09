8a54eaff8b3641da8ed1b6d6196ec1829e685cb3
gabrieloarcas <gabriel.oarcas@hotmail.com>
2020-05-09 22:08:55 +0100

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = document.getElementsByClassName("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+let showModal = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", showModal);
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+modalImg.onclick = function () {
+  modal.style.display = "none";
+};
